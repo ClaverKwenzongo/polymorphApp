a23291da297ac5615bc51ff581118c811c1173f2
ClaverKwenzongo <ClaverKwenzongo@DESKTOP-H3HP192>
2022-04-04 22:16:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace polymorphApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Vehicle vehicle = new Vehicle();
+            vehicle.Drive();
+            vehicle.Stop();
+            vehicle.Turn();
+
+            Aeroplane aeroplane = new Aeroplane();
+            aeroplane.Drive();
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphApp
+{
+    internal class Vehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Drive");
+        }
+
+        public void Turn()
+        {
+            Console.WriteLine("Turn");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stop");
+        }
+    }
+
+    class Aeroplane : Vehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("I am flying");
+        }
+    }
+}
